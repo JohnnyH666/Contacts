c95f5b7f03929ed72161fd892fba69dfeb64c68a
JohnnyH666 <johnnyhuanghcj@gmail.com>
2024-11-23 16:14:45 +0800

    fix new

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { Paper, Typography} from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
